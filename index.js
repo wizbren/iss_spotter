61a2c86f06f568b4c4a9c9b43aa198d0b8e6eedc
Brendan Brown <brendan94brown@gmail.com>
2025-04-10 22:00:07 -0700

    Added longitude/latitude functionality, completing second requirement for ISS project

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const fetchMyIP = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 /*fetchMyIP((error, IP) => {
   if (error) {
     console.log("Error fetching IP", error);
   } else {
     console.log("Your IP address is: ", IP);
+  }
+});*/
+
+
+const sampleVanIP = "66.183.83.48";
+
+/*fetchCoordsByIP(sampleVanIP, (error, coords) => {
+  if (error) {
+    console.log("Error fetching coordinates", error);
+  } else {
+    console.log("Coordinates: ", coords);
   }
 });*/
--- iss.js
+++ iss.js
@@ -6,8 +6,7 @@ const fetchMyIP = function(callback) {
     if (error) return callback(error, null);
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
+      return callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);;
     }
     const bodyObj = JSON.parse(body);
     const ip = bodyObj.ip;
@@ -15,5 +14,31 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  const url = `https://ipwho.is/${ip}`;
 
-module.exports = { fetchMyIP };
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      return callback(Error(`Status Code ${response.statusCode} while fetching coordinates: ${body}`), null);
+    }
+    const bodyObj = typeof body === 'string' ? JSON.parse(body) : body;
+
+    if (!bodyObj.success) {
+      const message = `Success status was ${bodyObj.success}. Server message says: ${bodyObj.message} when fetching for IP ${ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    const coords = {
+      latitude: bodyObj.latitude,
+      longitude: bodyObj.longitude
+    };
+
+    callback(null, coords);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
