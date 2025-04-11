e8e9328e8a3a68e7fcefb3193153f9653fd551f9
Brendan Brown <brendan94brown@gmail.com>
2025-04-10 19:46:23 -0700

    Implemented initial ISS I requirements, IP pull successful

---
+++ index.js
@@ -0,0 +1,9 @@
+const fetchMyIP = require('./iss');
+
+/*fetchMyIP((error, IP) => {
+  if (error) {
+    console.log("Error fetching IP", error);
+  } else {
+    console.log("Your IP address is: ", IP);
+  }
+});*/
---
+++ iss.js
@@ -0,0 +1,19 @@
+const needle = require('needle');
+
+
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const IP = body.IP;
+    callback(null, IP);
+  });
+};
+
+
+module.exports = { fetchMyIP };
